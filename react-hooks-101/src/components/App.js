d1c3d03b4f16da180003a432307b6b82faa9b424
yoshihirohakuno <69379361+yoshihirohakuno@users.noreply.github.com>
2020-11-25 23:37:22 +0900

    Add files via upload

---
+++ react-hooks-101/src/components/App.js
@@ -0,0 +1,18 @@
+import React,{ useReducer } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EventForm from './EventForm'
+import Events from './Events'
+import reducer from '../reducers'
+
+const App = () => {
+  const [state,dispatch] = useReducer(reducer,[])
+
+  return(
+    <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}/>
+        <Events state={state} dispatch={dispatch}/>
+    </div>
+  )
+}
+
+export default App;
---
+++ react-hooks-101/src/reducers/index.js
@@ -0,0 +1,32 @@
+import {
+    CREATE_EVENT,
+    DELETE_ALL_EVENTS,
+    DELETE_EVENT
+} from '../actions'
+
+//action = {
+//    type:'CREATE_EVENT',
+//      title:'2020東京オリンピックのお知らせ',
+//     body:'2020年に東京でオリンピックを開催します！'
+// }
+
+const events = (state = [], action) =>{
+    switch(action.type){
+        case CREATE_EVENT:
+            const event = {title:action.title, body:action.body}
+            const length = state.length
+            const id = length === 0 ? 1 : state[length - 1].id + 1
+             
+            return [...state,{id, ...event}]
+
+        case DELETE_EVENT:
+            return state.filter(event => event.id !== action.id)
+        case DELETE_ALL_EVENTS:
+            return []
+
+        default:
+            return state
+    }
+}
+
+export default events
